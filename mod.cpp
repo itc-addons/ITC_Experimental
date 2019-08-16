3f8e746fb0a6684bfd5b4da7798e1ec1cb022d41
Andrew Somerville <asmsandrew@googlemail.com>
2019-08-17 00:04:00 +0100

    Initial Commit for Structure

---
+++ mod.cpp
@@ -0,0 +1,14 @@
+/// Name of your mod
+name = "ITC Experimental";
+/// Picture displayed from the expansions menu/ Optimal size is 2048x1024, other sizes work too
+picture = "itc-combo.paa";
+/// Display next to the item added by the mod
+logoSmall = "itc-combo.paa";
+/// Logo displayed in the main menu
+logo = "itc-combo.paa";
+/// When the mouse is over, in the main menu
+logoOver = "itc-combo.paa";
+/// Website URL, that can accessed from the expansions menu
+action = "https://github.com/itc-addons/ITC_Experimental";
+/// Overview text, displayed from the extension menu
+overview = "ITC Experimental";
